3467a32c15e9d8e9c6a44588f6e87325064fb6a4
Celinda Cueto <71739893+celycueto-ux@users.noreply.github.com>
2020-10-08 16:55:24 -0500

    Add files via upload

---
+++ src/HooksApp.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+export const HooksApp = () => {
+
+    return (
+        <div>
+
+            <h1>Hola mundo</h1>
+        </div>
+    );
+
+
+
+}
+
+export default HooksApp;
